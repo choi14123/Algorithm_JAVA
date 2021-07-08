9f9c4d9159d2270ebf571de2ebfc9980e81812ce
Choi Chang Hwan <ckdghks5565@gmail.com>
2021-07-08 15:53:34 +0900

    add day 07 08

---
+++ project_08/NUM_01.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NUM_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        int M;
+        if(C <= B) {
+            System.out.println(-1);
+        }
+        else {
+            M = A / (C - B) + 1;
+            System.out.println(M);
+            }
+
+
+    }
+}
---
+++ project_08/NUM_02.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class NUM_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int V = sc.nextInt();
+
+        int D = (V - B) / (A - B);
+        if ((V - B) % (A - B) != 0) {
+            D++;
+        }
+        System.out.println(D);
+    }
+}
+
