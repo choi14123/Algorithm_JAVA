091993f5ffb024ac804106d34c232438e079eed0
Choi Chang Hwan <ckdghks5565@gmail.com>
2021-07-10 17:16:11 +0900

    add day 07 10

---
+++ project_08/NUM_04.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class NUM_04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int count = 0, hap = 0, min = 0;
+
+
+        for (int i = A ; i <= B; ++i) {
+            int j;
+            for (j = 2; j * j <= i; ++j)
+                if (i % j == 0)
+                    break;
+            if ((j*j > i) && (i != 1)) {
+                hap += i;
+                if (count == 0)
+                    min = i;
+                count += 1;
+            }
+        }
+
+        if (count == 0)
+            System.out.println(-1);
+        else {
+            System.out.println(hap);
+            System.out.println(min);
+        }
+    }
+}
+
+
