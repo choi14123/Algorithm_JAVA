219fc0a86bc83719db3bc96942f81a7e1d1eb364
Choi Chang Hwan <ckdghks5565@gmail.com>
2021-07-12 17:20:40 +0900

    add day 07 12

--- project_08/NUM_06.java
+++ project_08/NUM_06.java
@@ -1,14 +1,27 @@
-// 재귀함수 팩토리얼
+// 기본수학2 직각삼각형
 import java.util.Scanner;
 
-public class NUM_05 {
+public class NUM_06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int temp = 1;
-        int N = sc.nextInt();
-        for (int i = N; i >= 1; i--){
-            temp = temp * i;
+        int N = 1;
+        while (N == 1) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int C = sc.nextInt();
+
+            if (A == 0 && B == 0 && C == 0) break;
+
+            double result = Math.pow(A, 2);
+            double result1 = Math.pow(B, 2);
+            double result2 = Math.pow(C, 2);
+
+            if (result + result1 == result2) {
+                System.out.println("right");
+            }
+            else {
+                System.out.println("wrong");
+            }
         }
-        System.out.println(temp);
     }
 }
